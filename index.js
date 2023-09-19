5cdf5c50adcff2140b41585b821f99d959adc4b1
kakil <kakil@me.com>
2023-09-19 14:35:35 +0000

    Completed the Request Header Parser Microservice

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+const os = require('os');
+const accepts = require('accepts');
+const useragent = require('useragent');
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -28,3 +32,48 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// Get IP Address
+function getIpAddress() {
+  const ifaces = os.networkInterfaces();
+  let ipAddress = null;
+
+  Object.keys(ifaces).forEach((ifname) => {
+    const iface = ifaces[ifname].find((info) => !info.internal && info.family === 'IPv4');
+    if (iface) {
+      ipAddress = iface.address;
+      return;
+    }
+  });
+
+  return ipAddress;
+}
+
+// user language
+function getUserLanguages(req) {
+  const accept = accepts(req);
+
+  const preferredLanguages = accept.languages();
+
+  return preferredLanguages;
+}
+
+// user software
+function getUserSoftware(req) {
+
+  const agent = useragent.parse(req.headers['user-agent']);
+
+  return agent.toString();
+}
+
+
+// Route for IP Address
+app.get('/api/whoami', (req, res) => {
+  console.log(getUserSoftware(req));
+  const ipAddress = getIpAddress();
+  const userLanguage = getUserLanguages(req);
+  const userSoftware = getUserSoftware(req);
+
+  res.json({ ipaddress: ipAddress, language: userLanguage, software: userSoftware });
+});
+
